7c9af74f2ade8653dbd62b5be53cb2d5a21a2287
luckyluck4989 <luckyluck4989@gmail.com>
2021-12-21 00:47:46 +0900

    remove test

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,7 +2,6 @@ var SCG20Token = artifacts.require("./SCG20Token.sol");
 var SimpleCrabGame = artifacts.require("./SimpleCrabGame.sol");
 
 module.exports = async function(deployer) {
-  //await deployer.deploy(SCG20Token);
+  await deployer.deploy(SCG20Token);
   await deployer.deploy(SimpleCrabGame, SCG20Token.address);
-  await deployer.deploy(SimpleCrabGame, "0x25E3da850910D346bdf34Ee1CBd3ea82ced24429");
 };
